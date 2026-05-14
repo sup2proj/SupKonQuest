1c5ca9496b4027e75eaf60f982fae68e5b6e1548
Stanislas berlaud <157527948+StanislasBerlaud@users.noreply.github.com>
2026-05-14 15:16:49 +0200

    del keyboard arrows management on scene LaunchingLocalNewGame

--- Assets/Scripts/Manager/LaunchingLocalNewGame.cs
+++ Assets/Scripts/Manager/LaunchingLocalNewGame.cs
@@ -5,196 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class LaunchingLocalNewGameManager : MonoBehaviour
 {
-    [Header("Menu Navigation")]
-    public TextMeshProUGUI[] rowTitles; 
-    public float normalSize = 40f;
-    public float selectedSize = 50f;
-    private int currentIndex = 0;
-
     [Header("Map Settings")]
     public Image mapImageDisplay;
     public TextMeshProUGUI mapNameDisplay;
-    public TextMeshProUGUI mapLeftArrow;
-    public TextMeshProUGUI mapRightArrow;
     public Sprite[] mapImages;
     public string[] mapNames;
     private int currentMapIndex = 0;
 
     [Header("AI Count Settings")]
     public TextMeshProUGUI aiCountDisplay;
-    public TextMeshProUGUI aiLeftArrow;
-    public TextMeshProUGUI aiRightArrow;
     private int aiCount = 1;
     public int maxAi = 7;
 
     [Header("Difficulty Settings")]
     public TextMeshProUGUI diffDisplay;
-    public TextMeshProUGUI diffLeftArrow;
-    public TextMeshProUGUI diffRightArrow;
     private string[] difficultiesEn = { "Easy", "Medium", "Hard" };
     private string[] difficultiesFr = { "Facile", "Moyen", "Difficile" };
     private int currentDiffIndex = 0;
 
-    [Header("Arrow Colors")]
-    public Color normalArrowColor = Color.grey;
-    public Color selectedArrowColor = Color.white;
-
     void Start()
     {
         UpdateDisplays();
-        UpdateVisuals();
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (currentIndex == 0)
-            {
-                currentIndex = 1;
-            }
-            else if (currentIndex == 1)
-            {
-                currentIndex = 2;
-            }
-            else if (currentIndex == 2)
-            {
-                currentIndex = 3;
-            }
-            else if (currentIndex == 3 || currentIndex == 4)
-            {
-                currentIndex = 0;
-            }
-            UpdateVisuals();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (currentIndex == 0)
-            {
-                currentIndex = 3;
-            }
-            else if (currentIndex == 1)
-            {
-                currentIndex = 0;
-            }
-            else if (currentIndex == 2)
-            {
-                currentIndex = 1;
-            }
-            else if (currentIndex == 3 || currentIndex == 4)
-            {
-                currentIndex = 2;
-            }
-            UpdateVisuals();
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (currentIndex == 0)
-            {
-                NextMap();
-            }
-            else if (currentIndex == 1)
-            {
-                IncreaseAi();
-            }
-            else if (currentIndex == 2)
-            {
-                NextDifficulty();
-            }
-            else if (currentIndex == 3)
-            {
-                currentIndex = 4;
-                UpdateVisuals();
-            }
-            else if (currentIndex == 4)
-            {
-                currentIndex = 3;
-                UpdateVisuals();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if (currentIndex == 0)
-            {
-                PreviousMap();
-            }
-            else if (currentIndex == 1)
-            {
-                DecreaseAi();
-            }
-            else if (currentIndex == 2)
-            {
-                PreviousDifficulty();
-            }
-            else if (currentIndex == 3)
-            {
-                currentIndex = 4;
-                UpdateVisuals();
-            }
-            else if (currentIndex == 4)
-            {
-                currentIndex = 3;
-                UpdateVisuals();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            if (currentIndex == 3)
-            {
-                StartGame();
-            }
-            else if (currentIndex == 4)
-            {
-                Back();
-            }
-        }
-    }
-
-    void UpdateVisuals()
-    {
-        for (int i = 0; i < rowTitles.Length; i++)
-        {
-            if (i == currentIndex)
-            {
-                rowTitles[i].fontSize = selectedSize;
-            }
-            else
-            {
-                rowTitles[i].fontSize = normalSize;
-            }
-        }
-
-        mapLeftArrow.color = normalArrowColor; 
-        mapRightArrow.color = normalArrowColor;
-        
-        aiLeftArrow.color = normalArrowColor; 
-        aiRightArrow.color = normalArrowColor;
-        
-        diffLeftArrow.color = normalArrowColor; 
-        diffRightArrow.color = normalArrowColor;
-
-        if (currentIndex == 0)
-        {
-            mapLeftArrow.color = selectedArrowColor; 
-            mapRightArrow.color = selectedArrowColor;
-        }
-        else if (currentIndex == 1)
-        {
-            aiLeftArrow.color = selectedArrowColor; 
-            aiRightArrow.color = selectedArrowColor;
-        }
-        else if (currentIndex == 2)
-        {
-            diffLeftArrow.color = selectedArrowColor; 
-            diffRightArrow.color = selectedArrowColor;
-        }
-    }
-
-    void NextMap()
+    public void NextMap()
     {
         currentMapIndex++;
         if (currentMapIndex > mapNames.Length - 1)
@@ -204,7 +38,7 @@ public class LaunchingLocalNewGameManager : MonoBehaviour
         UpdateDisplays();
     }
     
-    void PreviousMap()
+    public void PreviousMap()
     {
         currentMapIndex--;
         if (currentMapIndex < 0)
@@ -214,7 +48,7 @@ public class LaunchingLocalNewGameManager : MonoBehaviour
         UpdateDisplays();
     }
     
-    void IncreaseAi()
+    public void IncreaseAi()
     {
         aiCount++;
         if (aiCount > maxAi)
@@ -224,7 +58,7 @@ public class LaunchingLocalNewGameManager : MonoBehaviour
         UpdateDisplays();
     }
     
-    void DecreaseAi()
+    public void DecreaseAi()
     {
         aiCount--;
         if (aiCount < 0)
@@ -234,7 +68,7 @@ public class LaunchingLocalNewGameManager : MonoBehaviour
         UpdateDisplays();
     }
     
-    void NextDifficulty()
+    public void NextDifficulty()
     {
         currentDiffIndex++;
         
@@ -246,7 +80,7 @@ public class LaunchingLocalNewGameManager : MonoBehaviour
         UpdateDisplays();
     }
     
-    void PreviousDifficulty()
+    public void PreviousDifficulty()
     {
         currentDiffIndex--;
         
@@ -280,12 +114,12 @@ public class LaunchingLocalNewGameManager : MonoBehaviour
         }
     }
 
-    void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("MainMenu"); 
     }
 
-    void Back()
+    public void Back()
     {
         SceneManager.LoadScene("MainMenu"); 
     }
