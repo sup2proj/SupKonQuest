05f292606d1ea1342c95b21ca58bdd818dabe30c
Stanislas berlaud <157527948+StanislasBerlaud@users.noreply.github.com>
2026-05-14 13:55:19 +0200

    del keyboard arrows management on scene Options

--- Assets/Scripts/Manager/OptionsManager.cs
+++ Assets/Scripts/Manager/OptionsManager.cs
@@ -32,17 +32,6 @@ public class OptionsManager : MonoBehaviour
     public Toggle fullscreenToggle;
     public TextMeshProUGUI fullscreenText;
 
-    [Header("Colors")]
-    public Color normalColor = Color.grey;
-    public Color selectedColor = Color.white;
-    private GameObject lastSelectedRow;
-
-    [Header("ButtonBack")]
-    public GameObject btnBack;
-    public TextMeshProUGUI btnBackText;
-    public float normalSize = 40f;
-    public float selectedSize = 50f;
-
     void Start()
     {
         musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
@@ -58,78 +47,7 @@ public class OptionsManager : MonoBehaviour
         UpdateLanguageText();
         UpdateResolutionText();
 
-        ResetArrowsColor();
         EventSystem.current.SetSelectedGameObject(musicSlider.gameObject);
-    }
-
-    void Update()
-    {
-        GameObject selectedObj = EventSystem.current.currentSelectedGameObject;
-        
-        if (selectedObj != lastSelectedRow)
-        {
-            ResetArrowsColor();
-
-            if (selectedObj == languageRow)
-            {
-                if (langLeftArrow != null) langLeftArrow.color = selectedColor;
-                if (langRightArrow != null) langRightArrow.color = selectedColor;
-            }
-            else if (selectedObj == resolutionRow)
-            {
-                if (resLeftArrow != null) resLeftArrow.color = selectedColor;
-                if (resRightArrow != null) resRightArrow.color = selectedColor;
-            }
-            else if (selectedObj == fullscreenToggle.gameObject)
-            {
-                if (fullscreenText != null) fullscreenText.color = selectedColor;
-            }
-            else if (selectedObj == btnBack.gameObject)
-            {
-                if (btnBackText != null) 
-                {
-                    btnBackText.color = selectedColor;
-                }
-            }
-            UpdateTextSize(selectedObj == btnBack);
-
-            lastSelectedRow = selectedObj;
-        }
-
-        if (selectedObj != null)
-        {
-            if (selectedObj == languageRow)
-            {
-                if (Input.GetKeyDown(KeyCode.LeftArrow)) PreviousLanguage();
-                else if (Input.GetKeyDown(KeyCode.RightArrow)) NextLanguage();
-            }
-            else if (selectedObj == resolutionRow)
-            {
-                if (Input.GetKeyDown(KeyCode.LeftArrow)) PreviousResolution();
-                else if (Input.GetKeyDown(KeyCode.RightArrow)) NextResolution();
-            }
-        }
-    }
-
-    void UpdateTextSize(bool isSelected)
-    {
-        if (btnBackText == null)
-            return;
-
-        if (isSelected)
-            btnBackText.fontSize = selectedSize;
-        else
-            btnBackText.fontSize = normalSize;
-    }
-
-    void ResetArrowsColor()
-    {
-        if (langLeftArrow != null) langLeftArrow.color = normalColor;
-        if (langRightArrow != null) langRightArrow.color = normalColor;
-        if (resLeftArrow != null) resLeftArrow.color = normalColor;
-        if (resRightArrow != null) resRightArrow.color = normalColor;
-        if (fullscreenText != null) fullscreenText.color = normalColor;
-        if (btnBackText != null) btnBackText.color = normalColor;
     }
 
     public void OnMusicVolumeChanged(float value)
@@ -159,7 +77,6 @@ public class OptionsManager : MonoBehaviour
         else
             audioMixer.SetFloat("SFXVolume", Mathf.Log10(value) * 80f);
     }
-
 
     public void NextLanguage()
     {
