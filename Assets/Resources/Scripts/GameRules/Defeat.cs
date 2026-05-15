a2426643733b22624512f103a311b3a3b4942ba3
Louis7034 <louis37.bobet@gmail.com>
2026-05-16 00:22:17 +0200

    Create file for gameflow

---
+++ Assets/Resources/Scripts/GameRules/Defeat.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Defeat : MonoBehaviour
+{
+    // Quand un joueur n'a plus de structures, il perd la partie
+    
+    // SI un joueur déco alors ses structures deviennent neutre et on destroy toutes ses unités
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Scripts/GameRules/StructureAttribution.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class StructureAttribution : MonoBehaviour
+{
+    // Attribution des camps au début de parties 
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
