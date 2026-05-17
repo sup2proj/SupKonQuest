8db8e16b06fbba48a15637a48dab348715e46c41
Stanislas berlaud <157527948+StanislasBerlaud@users.noreply.github.com>
2026-05-17 16:21:54 +0200

    add matchmaking logic in MultiplayerMenuManager

--- Assets/Scripts/Network/MultiplayerMenuManager.cs
+++ Assets/Scripts/Network/MultiplayerMenuManager.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using System.Collections.Generic;
 
 public class MultiplayerMenuManager : MonoBehaviour
 {
@@ -41,5 +44,40 @@ public class MultiplayerMenuManager : MonoBehaviour
     public void BackToLogin()
     {
         SceneManager.LoadScene("LoginScene"); 
+    }
+
+    public async void QuickMatchmaking()
+    {
+        if (statusText != null) statusText.text = "Recherche d'une partie en cours...";
+        try
+        {
+            string myName = PlayerPrefs.GetString("PlayerName", "Joueur");
+            QuickJoinLobbyOptions options = new QuickJoinLobbyOptions
+            {
+                Player = new Unity.Services.Lobbies.Models.Player
+                {
+                    Data = new Dictionary<string, PlayerDataObject>
+                    {
+                        { "PlayerName", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, myName) },
+                        { "IsReady", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, "False") }
+                    }
+                }
+            };
+
+            Lobby joinedLobby = await LobbyService.Instance.QuickJoinLobbyAsync(options);
+            Debug.Log("Partie trouvée ! Connexion en tant que Client.");
+            if (statusText != null) statusText.text = "Partie trouvée !";
+            LobbyRoomManager.IsHost = false;
+            LobbyRoomManager.JoinedLobby = joinedLobby;
+            SceneManager.LoadScene("LobbyRoomScene");
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log("Aucune partie trouvée. Je suis l'Hôte ! (" + e.Message + ")");
+            if (statusText != null) statusText.text = "Aucune partie. Création d'un salon...";
+
+            LobbyRoomManager.IsHost = true;
+            SceneManager.LoadScene("LobbyRoomScene");
+        }
     }
 }
